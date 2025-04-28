4ace852bb5061ac002e22559bcf5ef0362f92db2
Szymon Zubek <szymon@Szymons-MBP.mynet>
2025-04-28 19:54:05 +0100

    Testing Obsidian.nvim

--- dotfiles/nvim/lua/plugins/obsidian.lua
+++ dotfiles/nvim/lua/plugins/obsidian.lua
@@ -1,25 +1,25 @@
 return {
-	"epwalsh/obsidian.nvim",
-	version = "*",
-	lazy = true,
-	ft = "markdown",
-	dependencies = {
-		"nvim-lua/plenary.nvim",
-	},
-	opts = {
-		workspaces = {
-			{
-				name = "Personal",
-				path = "~/Documents/Notes/Personal",
-			},
-		},
-		ui = {
-			enable = false,
-		},
-		templates = {
-			folder = "Templates",
-			date_format = "%d-%m-%Y",
-			time_format = "%H-%M",
-		},
-	},
+  "epwalsh/obsidian.nvim",
+  version = "*",
+  lazy = true,
+  ft = "markdown",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+  },
+  opts = {
+    workspaces = {
+      {
+        name = "Personal",
+        path = "~/Documents/Test",
+      },
+    },
+    ui = {
+      enable = false,
+    },
+    templates = {
+      folder = "Templates",
+      date_format = "%d-%m-%Y",
+      time_format = "%H-%M",
+    },
+  },
 }
